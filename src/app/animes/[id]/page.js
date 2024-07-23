8b73431dc04262d5506d11b88618b4b12a78f737
Bruninhu <brunoacristo13@gmail.com>
2024-07-23 20:29:36 -0300

    Update page.js

--- src/app/animes/[id]/page.js
+++
@@ -1,7 +0,0 @@
-export default async function Animes({params}) {
-    const response = await fetch("http://localhost:3000/api/" + params.id);
-    const data = await response.json();
-      return(
-        <p>id: {data.titulo}</p>
-    );
-}
