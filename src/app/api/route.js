c672e91b67abe65a1ef829d054abff6a90e79adf
Bruninhu <brunoacristo13@gmail.com>
2024-07-23 20:01:04 -0300

    Update route.js

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,41 +1,41 @@
 import { NextResponse } from "next/server";
 import { Link } from "react-router-dom";
 
-export const modsMinecraft = [
+export const animes = [
     {
         id: 1,
-        titulo: "Dragon Block C",
-        autor: "JinRyuu",
-        anoPublicacao: 2014,
+        titulo: "Dragon Ball Z",
+        autor: "Akira Toriyama",
+        anoPublicacao: 1989,
         genero: "Ação"
     },
     {
         id: 2,
-        titulo: "Mine Mine no Mi",
-        autor: "WyndFTWz",
-        anoPublicacao: 2014,
+        titulo: "One Piece",
+        autor: "Eiichiro Oda",
+        anoPublicacao: 1999,
         genero: "Aventura"
     },
     {
         id: 3,
-        titulo: "Mine Mine no Mi",
-        autor: "WyndFTWz",
-        anoPublicacao: 2014,
-        genero: "Aventura"
+        titulo: "Pokémon",
+        autor: "Satoshi Tajiri",
+        anoPublicacao: 1995,
+        genero: "Fantasia"
     },
     {
         id: 4,
-        titulo: "Mine Mine no Mi",
-        autor: "WyndFTWz",
+        titulo: "My Hero Academia",
+        autor: "Kōhei Horikoshi",
         anoPublicacao: 2014,
-        genero: "Aventura"
+        genero: "Super-heróis"
     },
     {
         id: 5,
-        titulo: "Mine Mine no Mi",
-        autor: "WyndFTWz",
-        anoPublicacao: 2014,
-        genero: "Aventura"
+        titulo: "Jujutsu Kaisen",
+        autor: "Gege Akutami",
+        anoPublicacao: 2018,
+        genero: "Ação"
     }
 ];
 
