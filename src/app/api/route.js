82a290a51ea9530f561842040e1a233caa0c08f5
Bruninhu <brunoacristo13@gmail.com>
2024-07-23 19:39:36 -0300

    Update route.js

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,2 +1,49 @@
 import { NextResponse } from "next/server";
 import { Link } from "react-router-dom";
+
+export const modsMinecraft = [
+    {
+        id: 1,
+        titulo: "Dragon Block C",
+        autor: "JinRyuu",
+        anoPublicacao: 2014,
+        genero: "Ação"
+        img: ""
+    },
+    {
+        id: 2,
+        titulo: "Mine Mine no Mi",
+        autor: "WyndFTWz",
+        anoPublicacao: 2014,
+        genero: "Aventura"
+        img: ""
+    },
+    {
+        id: 3,
+        titulo: "Mine Mine no Mi",
+        autor: "WyndFTWz",
+        anoPublicacao: 2014,
+        genero: "Aventura"
+        img: ""
+    },
+    {
+        id: 4,
+        titulo: "Mine Mine no Mi",
+        autor: "WyndFTWz",
+        anoPublicacao: 2014,
+        genero: "Aventura"
+        img: ""
+    },
+    {
+        id: 5,
+        titulo: "Mine Mine no Mi",
+        autor: "WyndFTWz",
+        anoPublicacao: 2014,
+        genero: "Aventura"
+        img: ""
+    }
+];
+
+export async function GET(){
+    return NextResponse.json(listaDeLivros)
+}
