7ca38e879035a46ba51366748ed852fdd6ea965c
Bruninhu <brunoacristo13@gmail.com>
2024-07-23 23:02:08 -0300

    Update route.js

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -36,6 +36,27 @@ export const animes = [
         autor: "Gege Akutami",
         anoPublicacao: 2018,
         genero: "Ação"
+    },
+    {
+        id: 6,
+        titulo: "Naruto",
+        autor: "Masashi Kishimoto",
+        anoPublicacao: 2007,
+        genero: "Aventura"
+    },
+    {
+        id: 7,
+        titulo: "Nanatsu no Taizai",
+        autor: "Nakaba Suzuki",
+        anoPublicacao: 2014,
+        genero: "Ação"
+    },
+    {
+        id: 8,
+        titulo: "One Punch-Man",
+        autor: "Yusuke Murata",
+        anoPublicacao: 2012,
+        genero: "Comédia"
     }
 ];
 
