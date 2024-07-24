60fb60e1e83fc6d49edd3550bfecbaa2986f4f4a
Bruninhu <brunoacristo13@gmail.com>
2024-07-24 13:03:30 -0300

    commit final arrumando o css

--- src/app/animes/[id]/page.js
+++ src/app/animes/[id]/page.js
@@ -7,16 +7,16 @@ export default async function Anime({params}) {
     const response = await fetch("http://localhost:3000/api/" + params.id);
     const data = await response.json();
     return(
-        <main className={styles.listAnime}>
+        <main className={styles.main2}>
             <div className={styles.card}>
                 <div className={styles.img}>
                     <Image className={styles.img} width={500} height={500} src={data.img} />
                 </div>
-                <div className={styles.detalhesProduto}>
+                <div className={styles.card}>
                 <h1 className={styles.title}>{data.titulo}</h1>
-                    <p className={styles.textopeq2}>{data.autor}</p>
-                    <p className={styles.textopeq2}>{data.anoPublicacao}</p>
-                    <p className={styles.textopeq2}>{data.genero}</p>
+                    <p className={styles.textopeq2}><strong>Autor: </strong>{data.autor}</p>
+                    <p className={styles.textopeq2}><strong>Ano: </strong>{data.anoPublicacao}</p>
+                    <p className={styles.textopeq2}><strong>Gênero: </strong>{data.genero}</p>
                 </div>
             </div>
         </main>
