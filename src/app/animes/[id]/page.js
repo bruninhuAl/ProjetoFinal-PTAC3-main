cef4e74c96b4aa9146e6ef56f5387bb80022c8f9
Bruninhu <brunoacristo13@gmail.com>
2024-07-24 12:43:31 -0300

    Implementação do componente link com o id do objeto 2

--- src/app/animes/[id]/page.js
+++ src/app/animes/[id]/page.js
@@ -1,10 +1,24 @@
 import Link from "next/link"
+import styles from "../../styles/main.module.css";
+import Image from "next/image";
 
 export default async function Anime({params}) {
 
     const response = await fetch("http://localhost:3000/api/" + params.id);
     const data = await response.json();
     return(
-        <p>id: {data.titulo}</p>
+        <main className={styles.listAnime}>
+            <div className={styles.card}>
+                <div className={styles.img}>
+                    <Image className={styles.img} width={500} height={500} src={data.img} />
+                </div>
+                <div className={styles.detalhesProduto}>
+                <h1 className={styles.title}>{data.titulo}</h1>
+                    <p className={styles.textopeq2}>{data.autor}</p>
+                    <p className={styles.textopeq2}>{data.anoPublicacao}</p>
+                    <p className={styles.textopeq2}>{data.genero}</p>
+                </div>
+            </div>
+        </main>
     );
 }
